fda3017b07951cfd92716b84222d73262ddc6f14
Gitdeon <Gitdeon@github.com>
2019-03-20 16:33:51 +0100

    init othello pit

--- othello_pit.py
+++ othello_pit.py
@@ -1,4 +1,4 @@
-l#!/usr/bin/env python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Tue Mar 19 10:40:18 2019
@@ -8,8 +8,8 @@ Created on Tue Mar 19 10:40:18 2019
 
 import Arena
 #from MCTS import MCTS
-from gobang.GobangGame import GobangGame, display
-from gobang.GobangPlayers import *
+from othello.OthelloGame import OthelloGame, display
+from othello.OthelloPlayers import *
 import numpy as np
 from utils import *
 
@@ -17,11 +17,11 @@ from utils import *
 use this script to play any two agents against each other, or play manually with
 any agent.
 """
-g = GobangGame(9)
+g = OthelloGame(8)
 
 # all players
 rp = RandomPlayer(g).play
-hp = HumanGobangPlayer(g).play
+hp = HumanOthelloPlayer(g).play
 
 arena_rp_hp = Arena.Arena(rp, hp, g, display=display)
 print(arena_rp_hp.playGames(2, verbose=True))
