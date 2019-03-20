59daab225dedf117395392cbc0d034c56b55d840
Gitdeon <Gitdeon@github.com>
2019-03-20 16:17:18 +0100

    Initialized repository

---
+++ Qlearning.py
@@ -0,0 +1,3 @@
+'''
+write a simple Q-learning player
+'''
---
+++ gobang_pit.py
@@ -0,0 +1,27 @@
+l#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 19 10:40:18 2019
+
+@author: Gideon
+"""
+
+import Arena
+#from MCTS import MCTS
+from gobang.GobangGame import GobangGame, display
+from gobang.GobangPlayers import *
+import numpy as np
+from utils import *
+
+"""
+use this script to play any two agents against each other, or play manually with
+any agent.
+"""
+g = GobangGame(9)
+
+# all players
+rp = RandomPlayer(g).play
+hp = HumanGobangPlayer(g).play
+
+arena_rp_hp = Arena.Arena(rp, hp, g, display=display)
+print(arena_rp_hp.playGames(20, verbose=True))
