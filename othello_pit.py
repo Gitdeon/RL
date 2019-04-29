9934bd5441eef006dad325888b1f6c4614958aad
Gitdeon <Gitdeon@github.com>
2019-04-29 19:04:59 +0200

    Modified othello to run experiments

--- othello_pit.py
+++ othello_pit.py
@@ -18,21 +18,29 @@ from Qlearning import QlearningPlayer
 from MCTS import MCTSPlayer
 
 cmp.compile("othello/OthelloPlayers.py")
-
 g = OthelloGame(4)
+totalp1wins, totalp2wins, totaldraws = [], [], []
 
-# p1 = MCSPlayer(g)
+#using random seeds 8-18
+for i in range(8,18):
+    np.random.seed(i)
+    #p1 = MCSPlayer(g)
 
-#p1 = QlearningPlayer(g)
-#p1.train(10000)
+    #p1 = QlearningPlayer(g)
+    #p1.train(10000)
 
-mcts_arguments = dotdict({'MCTSiterations': 100, 'exploration_param': 1.0})
-mcts = MCTSPlayer(g, mcts_arguments)
-p1 = lambda x: np.argmax(mcts.getActionProb(x, temp=0))
+    mcts_arguments = dotdict({'MCTSiterations': 200, 'exploration_param': 1.0})
+    mcts = MCTSPlayer(g, mcts_arguments)
+    p1 = lambda x: np.argmax(mcts.getActionProb(x, temp=0))
+    
+    p2 = GreedyOthelloPlayer(g)
+    #p2 = RandomPlayer(g)
+    
+    arena = Arena(p2.play, p1, g, display=display)
+    p1wins, p2wins, draws = arena.playGames(100, verbose=False)
+    totalp1wins.append(p1wins)
+    totalp2wins.append(p2wins)
+    totaldraws.append(draws)
 
-p2 = GreedyOthelloPlayer(g)
-# p2 = RandomPlayer(g)
-
-print("Othello")
-arena = Arena(p1, p2.play, g, display=display)
-print(arena.playGames(100, verbose=False))
+print('Player 1 wins: ', np.mean(totalp1wins), ' Player 2 wins:' , np.mean(totalp2wins), ' Draws: ', np.mean(totaldraws), '\nStd. wins: ', np.std(totalp1wins), '\nStd. losses: ', np.std(totalp2wins))
+    
